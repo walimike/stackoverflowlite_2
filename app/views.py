148bc545c85ac77045cd9a6c94379d1faa7cd959
walimike <walimike139@gmail.com>
2018-08-21 15:38:51 +0300

    user views a pecific question [feature:#159845309]

--- app/views.py
+++ app/views.py
@@ -8,13 +8,15 @@ app = create_app (config_name='development')
 # This code fetches all questions
 @app.route('/stackoverlow/api/v1/questions', methods=['GET'])
 def get_all_questions():
-    return jsonify({'questions':questions})
+    pass
 
 
 # This code fetches specific question
 @app.route('/stackoverlow/api/v1/questions/<int:qn_id>', methods=['GET'])
 def get_one_question(qn_id):
-    pass
+    for one_question in questions:
+        if one_question['qn_id'] == qn_id:
+            return jsonify({'question': one_question})
 
 
 # This is to post/add a question
